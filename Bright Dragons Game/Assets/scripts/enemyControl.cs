249078ff9b975f5dbce8dd66d6c3c91f032ae67e
Elizabeth Van Dyke <tamcatnm@gmail.com>
2018-10-23 15:35:54 -0600

    Add enemy chase control script

---
+++ Bright Dragons Game/Assets/scripts/enemyControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyControl : MonoBehaviour
+{
+
+    public int chaseRange;
+    public float speed;
+    private Transform target;
+    private float distance;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // set the target to be the player
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // calculate the distance between the enemy and the target
+        distance = Vector2.Distance(transform.position, target.position);
+
+        //if the player is close enough to the enemy chase
+        if(distance <= chaseRange)
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+	}
+}
