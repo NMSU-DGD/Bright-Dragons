0216f3da28a42e71eed5103b0f535d063433cf51
Troy McMillan <mcmitr@nmsu.edu>
2018-11-24 13:38:03 -0800

    Dragon now goes to enemies within range, and goes back to player when they're all destroyed.
    -added rigidbody and trigger to enemies(can be deleted, but are necessary for the current system)
    -otherwise the only changes were to the DragonControl script and dragon object

--- Bright Dragons Game/Assets/scripts/DragonControl.cs
+++ Bright Dragons Game/Assets/scripts/DragonControl.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class DragonControl : MonoBehaviour {
 
+    public GameObject target;
     public GameObject player;
     private Rigidbody2D body;
     public float speed;
     public int dragonHealth = 3;
-    public Transform enemy;
+    
     public int chaseRange;
     private Func<Rigidbody2D> DragBod;
 
@@ -17,36 +18,35 @@ public class DragonControl : MonoBehaviour {
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (transform.position.x > player.transform.position.x + 1.5f)
+        if (transform.position.x > target.transform.position.x + 1.5f)
         {
-            Vector2 toTarget = player.transform.position - transform.position;
+            Vector2 toTarget = target.transform.position - transform.position;
 
 
             transform.Translate(toTarget * speed * Time.deltaTime);
         }
-        else if (transform.position.x < player.transform.position.x - 1.5f)
+        else if (transform.position.x < target.transform.position.x - 1.5f)
         {
-            Vector2 toTarget = player.transform.position - transform.position;
+            Vector2 toTarget = target.transform.position - transform.position;
    
 
             transform.Translate(toTarget * speed * Time.deltaTime);
         }
-        else if (transform.position.y < player.transform.position.y - 1.5f)
+        else if (transform.position.y < target.transform.position.y - 1.5f)
         {
-            Vector2 toTarget = player.transform.position - transform.position;
+            Vector2 toTarget = target.transform.position - transform.position;
 
 
             transform.Translate(toTarget * speed * Time.deltaTime);
         }
-        else if (transform.position.y > player.transform.position.y + 1.5f)
+        else if (transform.position.y > target.transform.position.y + 1.5f)
         {
-            Vector2 toTarget = player.transform.position - transform.position;
+            Vector2 toTarget = target.transform.position - transform.position;
 
 
             transform.Translate(toTarget * speed * Time.deltaTime);
@@ -60,18 +60,39 @@ public class DragonControl : MonoBehaviour {
     void Update()
     {
         // calculate the distance between the enemy and the target
-        float distance = Vector2.Distance(transform.position, enemy.position);
+        //float distance = Vector2.Distance(transform.position, enemy.position);
 
         //if the player is close enough to the enemy chase
-        if (distance <= chaseRange)
-            transform.position = Vector2.MoveTowards(transform.position, enemy.position, speed * Time.deltaTime);
+        //if (distance <= chaseRange)
+          //  transform.position = Vector2.MoveTowards(transform.position, enemy.position, speed * Time.deltaTime);
 
-        if (dragonHealth <= 0 && distance <= chaseRange)
+        if (dragonHealth <= 0 && target != player)
         {
             //DragBod = this.GetComponent<Rigidbody2D>;
             this.GetComponent<Rigidbody2D>().isKinematic = false;
             speed = 0;
         }
+
+        if (target == null)
+        {
+            target = player;
+            dragonHealth = 3;
+        }
+    }
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            if (target == null || target == player)
+                target = other.gameObject;
+
+
+        } 
 
 
     }
